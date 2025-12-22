24d8bdf100ceea15166ab338c5470ffc73d99b77
UmsrAkne <sibata486@gmail.com>
2025-12-22 14:00:35 +0900

    init

---
+++ PromptStamp/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Prism.Mvvm;
+using PromptStamp.Utils;
+
+namespace PromptStamp.ViewModels;
+
+// ReSharper disable once ClassNeverInstantiated.Global
+public class MainWindowViewModel : BindableBase
+{
+    private readonly AppVersionInfo appVersionInfo = new ();
+
+    public string Title => appVersionInfo.Title;
+}
