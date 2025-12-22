3099c074f40c2b2449f9c4e9b2373f9453f5849a
UmsrAkne <sibata486@gmail.com>
2025-12-22 14:25:10 +0900

    Add basic UI structure and styling with data bindings
    - Introduce `CommonFontSize`, `CommonCornerRadius`, and `CommonBackgroundBrush` to shared resources.
    - Update `MainWindow.xaml` with grid-based layout including `TextBlock`, `ListBox`, `TextBox`, and data bindings.
    - Create `ImagePromptGroup` model and enhance `MainWindowViewModel` to support `CommonPrompt` and `ImagePromptGroups`.

---
+++ PromptStamp/Models/ImagePromptGroup.cs
@@ -0,0 +1,11 @@
+﻿using Prism.Mvvm;
+
+namespace PromptStamp.Models
+{
+    public class ImagePromptGroup : BindableBase
+    {
+        private string header;
+
+        public string Header { get => header; set => SetProperty(ref header, value); }
+    }
+}
--- PromptStamp/ViewModels/MainWindowViewModel.cs
+++ PromptStamp/ViewModels/MainWindowViewModel.cs
@@ -1,12 +1,27 @@
-﻿using Prism.Mvvm;
+﻿using System.Collections.ObjectModel;
+using Prism.Mvvm;
+using PromptStamp.Models;
 using PromptStamp.Utils;
 
 namespace PromptStamp.ViewModels;
 
-// ReSharper disable once ClassNeverInstantiated.Global
 public class MainWindowViewModel : BindableBase
 {
     private readonly AppVersionInfo appVersionInfo = new ();
 
+    private ObservableCollection<ImagePromptGroup> imagePromptGroups;
+
+    private string commonPrompt = string.Empty;
+
+    public string CommonPrompt { get => commonPrompt; set => SetProperty(ref commonPrompt, value); }
+
+    public ObservableCollection<ImagePromptGroup> ImagePromptGroups
+    {
+        get => imagePromptGroups;
+        set => SetProperty(ref imagePromptGroups, value);
+    }
+
     public string Title => appVersionInfo.Title;
 }
+
+// ReSharper disable once ClassNeverInstantiated.Global
